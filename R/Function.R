cdafe58c2c6d7cc9766915e5c1a2ac8a93d3c488
DianaYuanZhou <zhouyuan19961013@gmail.com>
2019-12-09 14:21:32 -0500

    Edit functions

--- R/Function.R
+++ R/Function.R
@@ -41,12 +41,10 @@ split <- function(objects, ratio, name1, name2) {
     names.in.list <- mget(c(name1, name2))
     for (name in names(objects)) {
         list2env(objects, envir = environment())
-        assign(paste0(name, ".", name1), subs(get(name), 
-            get(name1)))
-        assign(paste0(name, ".", name2), subs(get(name), 
-            get(name2)))
-        names.in.list.new <- mget(c(paste0(name, 
-            ".", name1), paste0(name, ".", name2)))
+        assign(paste0(name, ".", name1), subs(get(name), get(name1)))
+        assign(paste0(name, ".", name2), subs(get(name), get(name2)))
+        names.in.list.new <- mget(c(paste0(name, ".", name1), 
+            paste0(name, ".", name2)))
         names.in.list <- c(names.in.list, names.in.list.new)
     }
     return(names.in.list)
@@ -54,10 +52,8 @@ split <- function(objects, ratio, name1, name2) {
 
 # Objective function
 Obj.func <- function(Y, X, beta, step_size) {
-    g.value <- tcrossprod(t(Y - X %*% beta))/(2 * 
-        step_size)
-    # g.null <- tcrossprod(t(Y-
-    # mean(Y)))/(2*step_size)
+    g.value <- tcrossprod(t(Y - X %*% beta))/(2 * step_size)
+    # g.null <- tcrossprod(t(Y- mean(Y)))/(2*step_size)
     return(g.value)
 }
 
@@ -80,73 +76,66 @@ S <- function(lambda, X) {
 # Gradient
 Grad <- function(Y, X, parameters, lambda, step_size) {
     grad <- (-1) * t(X) %*% (Y - X %*% parameters)
-    grad.g <- (parameters - S(lambda, X = parameters - 
-        step_size * grad))/step_size
+    grad.g <- (parameters - S(lambda, X = parameters - step_size * 
+        grad))/step_size
     Grads <- mget(c("grad", "grad.g"))
     return(Grads)
 }
 
 # Backtrcking line search
-backtracking <- function(Y, X, parameters, grad, 
-    grad.g, lambda, step_size, constant) {
-    # list2env(Grads, envir = environment())
-    # list2env(grad, envir = environment())
-    # list2env(grad.g, envir = environment())
+backtracking <- function(Y, X, parameters, grad, grad.g, lambda, 
+    step_size, constant) {
+    # list2env(Grads, envir = environment()) list2env(grad, envir
+    # = environment()) list2env(grad.g, envir = environment())
     
-    while (Obj.func(Y, X, beta = parameters - 
-        step_size * grad.g, step_size = step_size) * 
-        step_size > Obj.func(Y, X, parameters, 
-        step_size = step_size) * step_size - 
-        step_size * t(grad) %*% grad.g + (step_size/2) * 
-        tcrossprod(t(grad.g))) {
+    while (Obj.func(Y, X, beta = parameters - step_size * grad.g, 
+        step_size = step_size) * step_size > Obj.func(Y, X, parameters, 
+        step_size = step_size) * step_size - step_size * t(grad) %*% 
+        grad.g + (step_size/2) * tcrossprod(t(grad.g))) {
         step_size <- constant * step_size
-        grad.g <- (parameters - S(lambda, (parameters - 
-            step_size * grad)))/step_size
+        grad.g <- (parameters - S(lambda, (parameters - step_size * 
+            grad)))/step_size
     }
     return(step_size)
 }
 
 # Update parameters
-update.parameters <- function(lambda, grad, parameters, 
-    step_size) {
-    parameters_new = S(lambda, parameters - step_size * 
-        grad)
+update.parameters <- function(lambda, grad, parameters, step_size) {
+    parameters_new = S(lambda, parameters - step_size * grad)
     return(parameters_new)
 }
 
 # Predict
 pred <- function(Y.test, X.test, parameters) {
     Y.test.hat <- X.test %*% parameters
-    test.error <- Obj.func(Y.test, X.test, parameters, 
-        step_size = 1)
+    test.error <- Obj.func(Y.test, X.test, parameters, step_size = 1)
     return(mget(c("Y.test.hat", "test.error")))
 }
 
 # Main algrithm
-ISTA0 <- function(X.subtrain, Y.subtrain, X.valid, 
-    Y.valid, epoch = 10000, patience, lambda) {
+ISTA0 <- function(X.subtrain, Y.subtrain, X.valid, Y.valid, epoch = 10000, 
+    patience, lambda) {
     # --------- Initializa parameter ---------
     p <- ncol(X.subtrain)
     parameters <- runif(p, -0.5, 0.5)
     step_size <- 0.001
-    valid.error.best <- Obj.func(Y.valid, X.valid, 
-        parameters, step_size)
+    valid.error.best <- Obj.func(Y.valid, X.valid, parameters, 
+        step_size)
     parameters.best <- parameters
     k = 0
     
-    # --------- Calculate gradient and update
-    # parameters -----------
+    # --------- Calculate gradient and update parameters
+    # -----------
     for (i in 1:epoch) {
-        Grads <- Grad(Y.subtrain, X.subtrain, 
-            parameters, lambda = lambda, step_size)
-        step_size_update <- backtracking(Y.subtrain, 
-            X.subtrain, Grads$grad, Grads$grad.g, 
-            parameters, lambda = lambda, step_size, 
-            constant = 1/2)
-        parameters <- update.parameters(lambda = lambda, 
-            Grads$grad, parameters, step_size_update)
-        valid.error <- Obj.func(Y.valid, X.valid, 
-            parameters, step_size)
+        Grads <- Grad(Y.subtrain, X.subtrain, parameters, lambda = lambda, 
+            step_size)
+        step_size_update <- backtracking(Y.subtrain, X.subtrain, 
+            Grads$grad, Grads$grad.g, parameters, lambda = lambda, 
+            step_size, constant = 1/2)
+        parameters <- update.parameters(lambda = lambda, Grads$grad, 
+            parameters, step_size_update)
+        valid.error <- Obj.func(Y.valid, X.valid, parameters, 
+            step_size)
         
         if (valid.error < valid.error.best) {
             valid.error.best <- valid.error
@@ -154,11 +143,9 @@ ISTA0 <- function(X.subtrain, Y.subtrain, X.valid,
         } else {
             k = k + 1
         }
-        # cat(i, valid.error, valid.error.best, k,
-        # '\n')
+        # cat(i, valid.error, valid.error.best, k, '\n')
         if (k == patience) {
-            return(mget(c("i", "valid.error.best", 
-                "parameters.best")))
+            return(mget(c("i", "valid.error.best", "parameters.best")))
         }
     }
     return(mget(c("i", "valid.error.best", "parameters.best")))
--- R/ISTA.main.R
+++ R/ISTA.main.R
@@ -13,75 +13,64 @@
 ##' @author Yuan Zhou, Lingsong Meng
 ##' @export
 
-# library(MASS) library(Metrics)
-# source('Function.R')
+# library(MASS) library(Metrics) source('Function.R')
 
-ISTA.main <- function(data.X, data.Y, scale = T,
-    lambda = 0.01, method = NULL, epoch = 10000,
-    patience = 50) {
-
-    stopifnot(!missing(data.X), !missing(data.Y),
-        is.matrix(data.X), is.matrix(data.Y),
-        ncol(data.Y) == 1)
-
+ISTA.main <- function(data.X, data.Y, scale = T, lambda = 0.01, 
+    method = NULL, epoch = 10000, patience = 50) {
+    
+    stopifnot(!missing(data.X), !missing(data.Y), is.matrix(data.X), 
+        is.matrix(data.Y), ncol(data.Y) == 1)
+    
     if (scale == T) {
         data.X <- scale(data.X)
         data.Y <- scale(data.Y)
     }
-
-    list2env(split(mget(c("data.X", "data.Y")),
-        0.8, "train", "test"), envir = environment())
+    
+    list2env(split(mget(c("data.X", "data.Y")), 0.8, "train", 
+        "test"), envir = environment())
     ISTA.result <- list()
-
+    
     if (is.null(method)) {
         for (i in 1:length(lambda)) {
-            parameters.best <- ISTA0(data.X.train,
-                data.Y.train, data.X.test, data.Y.test,
-                epoch = epoch, patience = patience,
+            parameters.best <- ISTA0(data.X.train, data.Y.train, 
+                data.X.test, data.Y.test, epoch = epoch, patience = patience, 
                 lambda = lambda[i])$parameters.best
-            Y.test.hat <- pred(data.Y.test, data.X.test,
-                parameters.best)$Y.test.hat
+            Y.test.hat <- pred(data.Y.test, data.X.test, parameters.best)$Y.test.hat
             test.mse <- mse(data.Y.test, Y.test.hat)
-            cat("Test MSE in test set with lambda",
-                lambda[i], "is", test.mse, "\n")
+            cat("Test MSE in test set with lambda", lambda[i], 
+                "is", test.mse, "\n")
         }
-        ISTA.result <- list(`Estimate coefficient` = parameters.best,
-            `MSE in test set` = test.mse)
+        ISTA.result <- list(`Estimate coefficient` = parameters.best, 
+            `MSE in test set` = test.mse, Prediction = Y.test.hat)
         print(parameters.best)
         return(ISTA.result)
     } else {
-        stopifnot(method %in% c("CV", "AIC",
-            "BIC"))
+        stopifnot(method %in% c("CV", "AIC", "BIC"))
         p <- ncol(data.X)
         result.list <- list()
-        # --------- Split datasets and find best
-        # labmda ------------
+        # --------- Split datasets and find best labmda ------------
         if ("CV" %in% method) {
             fold = 10
-            result.cv <- data.frame(lambda = seq(0,
-                0, len = length(lambda)), valid.error.mean = seq(0,
-                0, len = length(lambda)), epoch = seq(0,
-                0, len = length(lambda)))
+            result.cv <- data.frame(lambda = seq(0, 0, len = length(lambda)), 
+                valid.error.mean = seq(0, 0, len = length(lambda)), 
+                epoch = seq(0, 0, len = length(lambda)))
             valid.error.cv <- numeric(fold)
             training.epoch <- numeric(fold)
             for (i in 1:length(lambda)) {
-                data.train <- data.frame(data.X.train,
-                  data.Y.train)
-                data.train.s <- split.data.frame(data.train,
-                  factor(1:fold))
+                data.train <- data.frame(data.X.train, data.Y.train)
+                data.train.s <- split.data.frame(data.train, factor(1:fold))
                 for (j in 1:fold) {
-                  data.X.valid <- as.matrix(data.train.s[[j]][,
+                  data.X.valid <- as.matrix(data.train.s[[j]][, 
                     1:p])
-                  data.Y.valid <- as.matrix(data.train.s[[j]][,
+                  data.Y.valid <- as.matrix(data.train.s[[j]][, 
                     "data.Y.train"])
-                  data.X.subtrain <- as.matrix(data.train[-as.numeric(row.names(data.X.valid)),
+                  data.X.subtrain <- as.matrix(data.train[-as.numeric(row.names(data.X.valid)), 
                     1:p])
-                  data.Y.subtrain <- as.matrix(data.train[-as.numeric(row.names(data.X.valid)),
+                  data.Y.subtrain <- as.matrix(data.train[-as.numeric(row.names(data.X.valid)), 
                     "data.Y.train"])
-
-                  training.result <- ISTA0(data.X.subtrain,
-                    data.Y.subtrain, data.X.valid,
-                    data.Y.valid, epoch = epoch,
+                  
+                  training.result <- ISTA0(data.X.subtrain, data.Y.subtrain, 
+                    data.X.valid, data.Y.valid, epoch = epoch, 
                     patience = patience, lambda = lambda[i])
                   valid.error.cv[j] <- training.result$valid.error.best
                   training.epoch[j] <- training.result$i
@@ -90,89 +79,76 @@ ISTA.main <- function(data.X, data.Y, scale = T,
                 result.cv[i, "valid.error.mean"] <- mean(valid.error.cv)
                 result.cv[i, "epoch"] <- mean(training.epoch)
             }
-
-            cat("Best lambda under cross validation criteria is",
-                result.cv[which.min(result.cv$valid.error.mean),
+            
+            cat("Best lambda under cross validation criteria is", 
+                result.cv[which.min(result.cv$valid.error.mean), 
                   "lambda"], "\n")
             result.list[["CV"]] <- result.cv
         }
         if ("AIC" %in% method || "BIC" %in% method) {
-            list2env(split(list(data.X = data.X.train,
-                data.Y = data.Y.train), 0.75,
-                "subtrain", "valid"), envir = environment())
-            result.aic <- data.frame(lambda = seq(0,
-                0, len = length(lambda)), AIC = seq(0,
-                0, len = length(lambda)), epoch = seq(0,
-                0, len = length(lambda)))
-            result.bic <- data.frame(lambda = seq(0,
-                0, len = length(lambda)), BIC = seq(0,
-                0, len = length(lambda)), epoch = seq(0,
-                0, len = length(lambda)))
-
+            list2env(split(list(data.X = data.X.train, data.Y = data.Y.train), 
+                0.75, "subtrain", "valid"), envir = environment())
+            result.aic <- data.frame(lambda = seq(0, 0, len = length(lambda)), 
+                AIC = seq(0, 0, len = length(lambda)), epoch = seq(0, 
+                  0, len = length(lambda)))
+            result.bic <- data.frame(lambda = seq(0, 0, len = length(lambda)), 
+                BIC = seq(0, 0, len = length(lambda)), epoch = seq(0, 
+                  0, len = length(lambda)))
+            
             for (i in 1:length(lambda)) {
-                training.result <- ISTA0(data.X.subtrain,
-                  data.Y.subtrain, data.X.valid,
-                  data.Y.valid, epoch = epoch,
-                  patience = patience, lambda = lambda[i])
+                training.result <- ISTA0(data.X.subtrain, data.Y.subtrain, 
+                  data.X.valid, data.Y.valid, epoch = epoch, patience = patience, 
+                  lambda = lambda[i])
                 parameters.best <- training.result$parameters.best
-
+                
                 if ("AIC" %in% method) {
-                  aic <- tcrossprod(t(data.Y.valid -
-                    data.X.valid %*% parameters.best))/(nrow(data.X.valid) *
-                    var(data.Y.valid)) + 2 *
-                    (length(which(parameters.best ==
-                      0)))/nrow(data.X.valid)
+                  aic <- tcrossprod(t(data.Y.valid - data.X.valid %*% 
+                    parameters.best))/(nrow(data.X.valid) * var(data.Y.valid)) + 
+                    2 * (length(which(parameters.best == 0)))/nrow(data.X.valid)
                   result.aic[i, "lambda"] <- lambda[i]
                   result.aic[i, "AIC"] <- aic
                   result.aic[i, "epoch"] <- training.result$i
-
+                  
                 }
                 if ("BIC" %in% method) {
-                  bic <- tcrossprod(t(data.Y.valid -
-                    data.X.valid %*% parameters.best))/(nrow(data.X.valid) *
-                    var(data.Y.valid)) + log(nrow(data.X.valid)) *
-                    (length(which(parameters.best ==
+                  bic <- tcrossprod(t(data.Y.valid - data.X.valid %*% 
+                    parameters.best))/(nrow(data.X.valid) * var(data.Y.valid)) + 
+                    log(nrow(data.X.valid)) * (length(which(parameters.best == 
                       0)))/nrow(data.X.valid)
                   result.bic[i, "lambda"] <- lambda[i]
                   result.bic[i, "BIC"] <- bic
                   result.bic[i, "epoch"] <- training.result$i
-
+                  
                 }
             }
             if ("AIC" %in% method) {
-                cat("Best lambda under AIC criteria is",
-                  result.aic[which.min(result.aic$AIC),
-                    "lambda"], "\n")
+                cat("Best lambda under AIC criteria is", result.aic[which.min(result.aic$AIC), 
+                  "lambda"], "\n")
                 result.list[["AIC"]] <- result.aic
             }
             if ("BIC" %in% method) {
-                cat("Best lambda under BIC criteria is",
-                  result.bic[which.min(result.bic$BIC),
-                    "lambda"], "\n")
+                cat("Best lambda under BIC criteria is", result.bic[which.min(result.bic$BIC), 
+                  "lambda"], "\n")
                 result.list[["BIC"]] <- result.bic
             }
         }
-
-        # ---------- Using model in test data
-        # --------------
+        
+        # ---------- Using model in test data --------------
         result.mse <- c("MSE")
         for (med in method) {
             result <- result.list[[med]]
-            lambda.best <- result[which.min(result[,
-                2]), "lambda"]
-            parameters.best <- ISTA0(data.X.subtrain,
-                data.Y.subtrain, data.X.valid,
-                data.Y.valid, epoch = epoch,
-                patience = patience, lambda = lambda.best)$parameters.best
-            Y.test.hat <- pred(data.Y.test, data.X.test,
-                parameters.best)$Y.test.hat
+            lambda.best <- result[which.min(result[, 2]), "lambda"]
+            parameters.best <- ISTA0(data.X.subtrain, data.Y.subtrain, 
+                data.X.valid, data.Y.valid, epoch = epoch, patience = patience, 
+                lambda = lambda.best)$parameters.best
+            Y.test.hat <- pred(data.Y.test, data.X.test, parameters.best)$Y.test.hat
             test.mse <- mse(data.Y.test, Y.test.hat)
-            cat("Test MSE in test set with best lambda under",
+            cat("Test MSE in test set with best lambda under", 
                 med, "is", test.mse, "\n")
             print(parameters.best)
-            ISTA.result[[med]] <- list(`Estimate coefficient` = parameters.best,
-                `MSE in test set` = test.mse)
-
+            ISTA.result[[med]] <- list(`Estimate coefficient` = parameters.best, 
+                `MSE in test set` = test.mse, Prediction = Y.test.hat)
+            
         }
     }
     return(ISTA.result)
